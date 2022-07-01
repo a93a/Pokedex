dadcdbc2e7dd74603198459b62a60397c65f4202
unknown <aviamirswe@gmail.com>
2022-07-01 19:51:17 +0200

    +pokelist screen

---
+++ app/src/main/java/com/example/pokedex/ui/pokemonlist/PokemonListScreen.kt
@@ -0,0 +1,42 @@
+package com.example.pokedex.ui.pokemonlist
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.pokedex.R
+
+@Composable
+fun PokemonListScreen(
+    navController: NavController
+) {
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_international_pok_mon_logo),
+                contentDescription = "Pokemon",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(CenterHorizontally)
+            )
+            SearchBar(
+                hint = "Search",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+
+            }
+        }
+    }
+}
